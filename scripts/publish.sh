b14975b4c07b6b9e99372d9f7c44471dee458898
Jonathan LEI <xJonathan@outlook.com>
2021-08-27 07:58:14 +0000

    Change ADMIN_TOKEN to Worker secret

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,8 @@ mv ./wrangler.toml ./wrangler.backup.toml
 
 yarn envsub ./wrangler.backup.toml ./wrangler.toml
 
+echo "${ADMIN_TOKEN}" | yarn wrangler secret put ADMIN_TOKEN
+
 yarn wrangler publish
 
 mv ./wrangler.backup.toml ./wrangler.toml
